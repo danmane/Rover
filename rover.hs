c9e6741a48808c1d788d3255ed454ab1f78f6d1a
Daniel Mané <danmane@gmail.com>
2013-01-25 01:22:13 -0500

    added traversals which returns an infinite stream of steps to traversal. some refactoring
    added traversals :: Grid -> StdGen -> [Int]
    
    makeRover and other functions now take a Grid rather than a pair of ints

--- rover.hs
+++ rover.hs
@@ -8,6 +8,9 @@ data Grid = Grid { gridX :: Int, gridY :: Int, gSize :: Int } deriving (Show, Eq
 makeGrid :: Int -> Int -> Grid
 makeGrid x y = Grid x y (x*y)
 
+squareGrid :: Int -> Grid
+squareGrid x = Grid x x (x*x)
+
 data Coordinate = Coordinate {x :: Int, y :: Int} deriving (Show, Eq)
 
 data Rover = Rover { rgrid :: Grid
@@ -20,15 +23,16 @@ data Rover = Rover { rgrid :: Grid
 				   -- number of unvisited spots
 				   } deriving (Show, Eq)
 
-makeRover :: Int -> Int -> Rover
+makeRover :: Grid -> Rover
 -- Given the x and y bounds of the grid, make a new rover on that grid
-makeRover gx gy = Rover newGrid startCoord visited nRemaining where
-	newGrid = makeGrid gx gy
+makeRover grid = Rover grid startCoord visited nRemaining where
 	startCoord = Coordinate 0 0
-	nRemaining = gSize newGrid - 1
+	nRemaining = gSize grid - 1
 	visited = Set.singleton 0 
 
 moveRover :: Direction -> Rover -> Rover
+-- takes a direction and a rover, and constructs a new rover that's moved in 
+-- the given direction
 moveRover dir rover = Rover roverGrid newCoord newVisited newRemaining where
 	roverGrid = rgrid rover
 	newCoord = moveCoord (coord rover) roverGrid dir
@@ -60,78 +64,86 @@ randomDirs gen = map i2d $ randomRs (1,4) gen where
 		3 -> D
 		4 -> L
 
-traverse :: Int -> Int -> StdGen -> Int
+traverse :: Grid -> StdGen -> Int
 -- Takes an x and y bounds for the grid and a generator
 -- Returns the number of steps it took the rover to traverse the grid
-traverse x y gen = finalSteps where
-	rover = makeRover x y
+traverse grid gen = finalSteps where
+	rover = makeRover grid
 	directions = randomDirs gen
 	roverStates = scanl (flip moveRover) rover directions 
 	finalSteps = length $ takeWhile (\x -> nRemaining x > 0) roverStates
 
 
+traversals :: Grid -> StdGen -> [Int]
+-- given a Grid and a StdGen, return an infinite stream of traversals
+-- each int is the number of steps it took a rover to reach every space
+traversals grid gen = traverseStream grid (randomDirs gen) where
+	traverseStream :: Grid -> [Direction] -> [Int]
+	traverseStream grid dirs = nSteps : traverseStream grid newDirs where
+		roverStates = scanl (flip moveRover) (makeRover grid) dirs
+		(traversal, _) = span (\x -> nRemaining x > 0) roverStates
+		nSteps = length traversal
+		newDirs = drop nSteps dirs
+
 
 coord2Int :: Coordinate -> Grid -> Int
 coord2Int coord grid = (x coord) + (y coord) * (gridX grid)
 
--- Test Functions Below (yay TDD)
+------------------------------------
+-- Test Functions Below (yay TDD) --
+------------------------------------
 
 allTests :: Bool
-allTests = and [testMoveCoord, testCoord2Int, testMoveRover, testRandomDirs]
+allTests = and [testMoveCoord, testCoord2Int, testMoveRover, testRandomDirs,
+				testTraverse, testTraversals]
 
-testMoveCoord :: Bool
 testMoveCoord = and [t1, t2, t3, t4] where
 	t1 = (nc == Coordinate 0 1) where
 		testc = Coordinate 0 0 
-		testg = makeGrid 5 5
+		testg = squareGrid 5
 		nc = moveCoord testc testg U
 	t2 = (nc == Coordinate 1 5) where
 		testc = Coordinate 2 5
-		testg = makeGrid 7 7
+		testg = squareGrid 7
 		nc = moveCoord testc testg L
 	t3 = (nc == Coordinate 0 0) where
 		testc = Coordinate 4 0
-		testg = makeGrid 5 5
+		testg = squareGrid 5
 		nc = moveCoord testc testg R
 	t4 = (nc == Coordinate 3 6) where
 		testc = Coordinate 3 0
-		testg = makeGrid 7 7 
+		testg = squareGrid 7 
 		nc = moveCoord testc testg D
 
-testCoord2Int :: Bool
 testCoord2Int = and [tc1, tc2, tc3, tc4] where
 	tc1 = (idx == 0) where
-		idx = coord2Int (Coordinate 0 0) (makeGrid 5 5)
+		idx = coord2Int (Coordinate 0 0) (squareGrid 5)
 	tc2 = (idx == 5) where
-		idx = coord2Int (Coordinate 5 0) (makeGrid 6 6)
+		idx = coord2Int (Coordinate 5 0) (squareGrid 6)
 	tc3 = (idx == 8) where
-		idx = coord2Int (Coordinate 2 2) (makeGrid 3 3)
+		idx = coord2Int (Coordinate 2 2) (squareGrid 3)
 	tc4 = (idx == 4) where
-		idx = coord2Int (Coordinate 0 1) (makeGrid 4 4)
+		idx = coord2Int (Coordinate 0 1) (squareGrid 4)
 
-testMoveRover :: Bool
 testMoveRover = and [t1, t2] where
 	t1 = moved == Rover (makeGrid 2 2) (Coordinate 0 0) fullset 0 where
 		fullset = Set.fromList [0..3]
-		moved = foldr moveRover (makeRover 2 2) [U, R, D, L] 
+		moved = foldr moveRover (makeRover (squareGrid 2)) [U, R, D, L] 
 	t2 = (nRemaining moved == 6) && (visited moved == Set.fromList [0,1,2]) where
-		moved = foldr moveRover (makeRover 3 3) [R, R, R, L, L, R, R, L]
+		moved = foldr moveRover (makeRover(squareGrid 3)) [R, R, R, L, L, R, R, L]
 
 testRandomDirs = take 5 (randomDirs (mkStdGen 0)) == [R, L, R, U, D]
 
-testTraversal = and [t1, t2, t3] where
-	t1 = traverse 2 2 mkStdGen 100 == 11
-	t2 = traverse 2 2 mkStdGen 23  == 3
-	t3 = traverse 3 3 mkStdGen 7   == 9
+testTraverse = and [t1, t2, t3] where
+	t1 = traverse (squareGrid 2) (mkStdGen 100) == 11
+	t2 = traverse (squareGrid 2) (mkStdGen 23 ) == 3
+	t3 = traverse (squareGrid 3) (mkStdGen 7  ) == 9
 	-- Tested these by hand...
 
-
---move :: Grid -> Coordinate -> Move -> Coordinate
---move (Grid xmax ymax) (Coordinate x y) mv = Coordinate nx ny where
---	(mx, my) = case mv of 
---		U -> (x, y+1)
---		D -> (x, y-1)
---		L -> (x-1, y)
---		R -> (x+1, y)
---	nx = (mx + xmax) `mod` xmax
---	ny = (my + ymax) `mod` ymax
+testTraversals = and [t1, t2, t3, t4] where
+	t1 = take 4 (traversals (squareGrid 2) (mkStdGen 7)) == [3,4,9,6] 
+	-- t1 was verified by hand
+	-- t2-t4 just verify the head, not the sequence...
+	t2 = head (traversals (squareGrid 2) (mkStdGen 100)) == 11
+	t3 = head (traversals (squareGrid 2) (mkStdGen 23 )) == 3
+	t4 = head (traversals (squareGrid 3) (mkStdGen 7  )) == 9
