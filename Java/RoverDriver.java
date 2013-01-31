5354a240996a5ec46d5f6035b7ac936313c393ca
Daniel Mané <danmane@gmail.com>
2013-01-31 14:03:54 -0600

    Good version of the RoverDriver.
    Passes 20&10k test in 3s, 100&10k test in 2m42s

--- Java/RoverDriver.java
+++ Java/RoverDriver.java
@@ -24,45 +24,11 @@ class RoverDriver {
 
 		return traversalRecord;
 	}
-
-	public static int[] traversalsFromRandomArray(int size, int[] rArray){
-		Rover newRover;
-		int steps, target;
-		
-		newRover = new Rover(size,size);
-		steps = 0;
-		target = size*size;
-
-		while (newRover.getNumVisited() < target){
-			newRover.randomMove();
-			steps++;
-		}
-	}
-
-	private static int traverseRandomArray(int size, int start, int length, int[] rArray){
-		Rover newRover;
-		int steps, target, i;
-		
-		newRover = new Rover(size,size);
-		steps = 0;
-		target = size*size;
-
-		while (newRover.getNumVisited() < target && i < length){
-			move = Direction.int2Direction(rArray[i])
-			newRover.randomMove();
-			steps++;
-		}
-
-
-	}
-
 	public static void main(String[] args) {
 		int[] results;
-		results = traversals(100,1000);
+		results = traversals(100,10000);
 		for (int res : results) {
 			//System.out.println(res);
 		}
 	}
 }
-
-public class 
